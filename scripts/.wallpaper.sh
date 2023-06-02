6603ebe8e5c23a49198b8b20881e74f4058b0591
adamdad <adam-dad@hotmail.co.uk>
2023-06-02 01:41:56 +0100

    updated wallpapers path

--- scripts/.wallpaper.sh
+++ scripts/.wallpaper.sh
@@ -3,9 +3,9 @@
 while true; do
     H=$(date +%k)
     if (( 8 <= H && H < 17 )); then
-        feh --no-fehbg --bg-fill "/home/adam/solarsystem.png" &> /dev/null
+        feh --no-fehbg --bg-fill "/home/adam/wallpapers/solarsystem.png" &> /dev/null
     else
-        feh --no-fehbg --bg-fill "/home/adam/owl.webp" &> /dev/null
+        feh --no-fehbg --bg-fill "/home/adam/wallpapers/owl.webp" &> /dev/null
     fi
     sleep 2400
 done &
